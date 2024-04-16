be7577acd1d61ed7da50292c93d045b4e2edd95a
Mayank <mayankbansal125@gmail.com>
2024-04-17 01:00:30 +0530

    Add update and delete methods using ids

--- src/main/java/com/demo/quizserver/controller/QuestionController.java
+++ src/main/java/com/demo/quizserver/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package com.demo.quizserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,8 @@ import com.demo.quizserver.service.QuestionService;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("question")
@@ -21,21 +24,39 @@ public class QuestionController {
     @Autowired
     private QuestionService questionService;
 
-    // /question?category -> For getting questions that belong to specific category
-    @GetMapping("")
-    public List<Question> getCategory(@RequestParam String category) {
-        return questionService.getByCategory(category);
-    }
-
     // /question/allQuestions -> For getting all the questions from db
     @GetMapping("allQuestions")
     public List<Question> getQuestions() {
         return questionService.getAllQuestions();
     }
 
-    // /question/createQuestion -> For creating a new question
-    @PostMapping("createQuestion")
+    // /question?category -> For getting questions that belong to specific category
+    @GetMapping("/")
+    public List<Question> getCategory(@RequestParam String category) {
+        return questionService.getByCategory(category);
+    }
+
+    // /question/:id -> For reading a specific ques using id
+    @GetMapping("{id}")
+    public Question getQuestion(@PathVariable String id) {
+        return questionService.getQuestion(Integer.parseInt(id));
+    }
+
+    // /question/add -> For creating a new question
+    @PostMapping("add")
     public String addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
+    }
+
+    // /question/:id -> Put for updating the value for that id
+    @PutMapping("{id}")
+    public String updateQuestion(@PathVariable String id, @RequestBody Question question) {
+        return questionService.updateQuestion(Integer.parseInt(id), question);
+    }
+
+    // /question/:id -> For deleting the question with that id
+    @DeleteMapping("{id}")
+    public String deleteQuestion(@PathVariable String id) {
+        return questionService.deleteQuestion(Integer.parseInt(id));
     }
 }
--- src/main/java/com/demo/quizserver/service/QuestionService.java
+++ src/main/java/com/demo/quizserver/service/QuestionService.java
@@ -7,6 +7,7 @@ import com.demo.quizserver.dao.QuestionDao;
 import com.demo.quizserver.model.Question;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -18,13 +19,43 @@ public class QuestionService {
         return questionDao.findAll();
     }
 
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = questionDao.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            return new Question();
+        }
+    }
+
     public String addQuestion(Question question) {
-        System.out.println("Here's the question rec: " + question);
         questionDao.save(question);
         return "Successfully, created the question!";
     }
 
     public List<Question> getByCategory(String category) {
         return questionDao.findByCategory(category);
+    }
+
+    public String deleteQuestion(Integer id) {
+        Optional<Question> existingQuestion = questionDao.findById(id);
+        if (existingQuestion.isPresent()) {
+            Question question = existingQuestion.get();
+            questionDao.delete(question);
+            return "Question deleted successfully!";
+        } else {
+            return "Question doesn't exist";
+        }
+    }
+
+    public String updateQuestion(Integer id, Question newQuestion) {
+        Optional<Question> existingQuestion = questionDao.findById(id);
+        if (existingQuestion.isPresent()) {
+            newQuestion.setId(id);
+            questionDao.save(newQuestion);
+            return "Updated successfully!";
+        } else {
+            return "Question is not present!";
+        }
     }
 }
